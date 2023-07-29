694088e13208cb381d0f0a6dcfd67744d66b34af
gzuuus <gzuuus@protonmail.com>
2023-07-29 22:01:40 +0200

    debuggin

--- src/routes/[userPub]/+page.ts
+++ src/routes/[userPub]/+page.ts
@@ -2,20 +2,31 @@ import type { PageLoad } from './$types';
 import { get as getStore } from 'svelte/store';
 import ndkStore from '$lib/stores/provider';
 import { isNip05 } from '$lib/utils/helpers';
-import { NDKUser} from '@nostr-dev-kit/ndk';
+import { NDKUser } from '@nostr-dev-kit/ndk';
 
 export const load = (async ({ params }) => {
     const paramsArg = params.userPub;
     const ndk = getStore(ndkStore);
-    let npub: any;
-  
+    let npub: string;
+
+    console.log('paramsArg:', paramsArg);
+
     if (isNip05(paramsArg)) {
-      // npub = await nip05toPub(paramsArg);
-      let npubPromise = await NDKUser.fromNip05(paramsArg);
-      npub = npubPromise?.npub;
+        console.log('Is Nip05:', true);
+        try {
+            let npubPromise = await NDKUser.fromNip05(paramsArg);
+            console.log('npubPromise:', npubPromise);
+            npub = npubPromise?.npub;
+        } catch (error) {
+            console.error('Error fetching NDKUser:', error);
+        }
     } else {
-      npub = paramsArg;
+        console.log('Is Nip05:', false);
+        npub = paramsArg;
     }
+
+    console.log('Final npub:', npub);
+
     const ndkUser = ndk.getUser({ npub: npub });
 
     return {
