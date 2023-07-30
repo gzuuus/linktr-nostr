df730dc87ae4a934123fcd66c27f83441633b881
gzuuus <gzuuus@protonmail.com>
2023-07-30 21:47:28 +0200

    ndk updated

--- src/routes/[userPub]/+page.ts
+++ src/routes/[userPub]/+page.ts
@@ -2,31 +2,19 @@ import type { PageLoad } from './$types';
 import { get as getStore } from 'svelte/store';
 import ndkStore from '$lib/stores/provider';
 import { isNip05 } from '$lib/utils/helpers';
-import { NDKUser } from '@nostr-dev-kit/ndk';
+import { NDKUser} from '@nostr-dev-kit/ndk';
 
 export const load = (async ({ params }) => {
     const paramsArg = params.userPub;
     const ndk = getStore(ndkStore);
-    let npub: string;
-
-    console.log('paramsArg:', paramsArg);
-
+    let npub: any;
+  
     if (isNip05(paramsArg)) {
-        console.log('Is Nip05:', true);
-        try {
-            let npubPromise = await NDKUser.fromNip05(paramsArg);
-            console.log('npubPromise:', npubPromise);
-            npub = npubPromise?.npub;
-        } catch (error) {
-            console.error('Error fetching NDKUser:', error);
-        }
+      let npubPromise = await NDKUser.fromNip05(paramsArg);
+      npub = npubPromise?.npub;
     } else {
-        console.log('Is Nip05:', false);
-        npub = paramsArg;
+      npub = paramsArg;
     }
-
-    console.log('Final npub:', npub);
-
     const ndkUser = ndk.getUser({ npub: npub });
 
     return {
