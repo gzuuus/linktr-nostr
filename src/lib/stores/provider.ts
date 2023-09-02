2697523171594c2e84560ad5c0c7518d1ba328e3
gzuuus <gzuuus@protonmail.com>
2023-09-02 21:18:53 +0200

    .

--- src/lib/stores/provider.ts
+++ src/lib/stores/provider.ts
@@ -1,8 +1,17 @@
 import { writable } from 'svelte/store';
 import NDK from '@nostr-dev-kit/ndk';
+import type { NDKCacheAdapter } from '@nostr-dev-kit/ndk';
 import NDKCacheAdapterDexie from "@nostr-dev-kit/ndk-cache-dexie";
+import { browser } from '$app/environment';
 
-const dexieAdapter = new NDKCacheAdapterDexie({ dbName: 'noostry' });
+let cacheAdapter: NDKCacheAdapter | undefined;
+
+if (browser) {
+    cacheAdapter = new NDKCacheAdapterDexie({
+        dbName: "nostreeV01"
+    });
+}
+
 export const defaulRelaysUrls: string[] = [
     'wss://purplepag.es',
     'wss://relay.nostr.band',
@@ -15,7 +24,7 @@ export const defaulRelaysUrls: string[] = [
 
 const ndk = new NDK({
     explicitRelayUrls: defaulRelaysUrls,
-    cacheAdapter: dexieAdapter
+    cacheAdapter
 });
 
 const ndkStore = writable(ndk);
