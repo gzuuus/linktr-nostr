b3bc3512a18a09fa5cc7c6cb7f6aa6c03f948ea1
gzuuus <gzuuus@protonmail.com>
2023-09-23 15:47:54 +0200

    og images update

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,9 +1,7 @@
-import type { Kind } from "nostr-tools";
-
 export const ogImageUrl =
-  "https://cdn.nostr.build/i/d050cb88a4c0c315c38bd9909831b65ae1914a530320b1d4a3683e8c8a38baae.jpg";
+  "https://image.nostr.build/35a3489682170c6797e36559088b7622900b541bfd5a627d8e3fee841675f5da.png";
 export const ogLogoImg =
-  "https://cdn.nostr.build/i/p/d0e1421f97f86049f18ad7429fa2831c3ca606b5f945b7d646775a6183dc260b.jpg";
+  "https://image.nostr.build/7ece73d2d074d556eb8faad803af00b9a50f005f97648d047d2302ee9fe6f554.png";
 export const kindLinks = 30001;
 export const kindNotes = 1;
 export const kindArticles = 30023;
